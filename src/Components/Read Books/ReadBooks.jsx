9e80f7745b95d981dc81c170fe8b85fd71c6a1db
HamzaAryanSapnil <139946462+HamzaAryanSapnil@users.noreply.github.com>
2024-03-29 15:38:44 +0600

    if there is no book in wishlist and read list showing no books in wishlist button yet done

--- src/Components/Read Books/ReadBooks.jsx
+++ src/Components/Read Books/ReadBooks.jsx
@@ -1,4 +1,4 @@
-import  { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 import { getStoredReadingList } from "../../Utils/localStorage";
 
@@ -16,13 +16,19 @@ const ReadBooks = () => {
     }
   }, [books]);
 
-  return (
+  return readBooks.length === 0 ? (
+    <p>No books read yet</p>
+  ) : (
     <div>
       {readBooks.map((book) => (
         <div key={book.bookId}>
           <div className="card card-side bg-base-100 shadow-xl">
             <figure className="w-1/3">
-              <img className="h-64 w-48 rounded-3xl " src={book.image} alt="Movie" />
+              <img
+                className="h-64 w-48 rounded-3xl "
+                src={book.image}
+                alt="Movie"
+              />
             </figure>
             <div className="card-body">
               <div className="">
@@ -53,11 +59,17 @@ const ReadBooks = () => {
                 </p>
               </div>
               <div className="flex gap-x-3 justify-start items-center">
-                <button className="btn catagoryBtn rounded-full">Catagory: {book.category}</button>
-                <button className="btn ratingBtn rounded-full">Rating: {book.rating}</button>
-                <Link to={`/bookDetails/${book.bookId}`} >
-                <button className="btn signInBtn rounded-full">View Details</button>
-               </Link>
+                <button className="btn catagoryBtn rounded-full">
+                  Catagory: {book.category}
+                </button>
+                <button className="btn ratingBtn rounded-full">
+                  Rating: {book.rating}
+                </button>
+                <Link to={`/bookDetails/${book.bookId}`}>
+                  <button className="btn signInBtn rounded-full">
+                    View Details
+                  </button>
+                </Link>
               </div>
             </div>
           </div>
@@ -66,6 +78,5 @@ const ReadBooks = () => {
     </div>
   );
 };
-
 
 export default ReadBooks;
--- src/Components/WishList/Wishlist.jsx
+++ src/Components/WishList/Wishlist.jsx
@@ -14,70 +14,72 @@ const Wishlist = () => {
       );
       setWishBooks(booksInWishlist);
     }
-  
-    
-  }, [books])
-  
- 
+  }, [books]);
 
-  return (
+  return wishBooks.length === 0 ? (
+    <p>No books in wishlist</p>
+  ) : (
     <div>
-        {wishBooks.map(book => (
-            <section key={book.bookId} className="dark:bg-gray-100 dark:text-gray-800">
-            <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
-              <NavLink
-                rel="noopener noreferrer"
-                
-                className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-50"
-              >
-                <img
-                  
-                  src={book.image}
-                  alt=""
-                  className="object-cover h-64 w-full rounded-3xl sm:h-96 lg:col-span-3 dark:bg-gray-500"
-                />
-                <div className="p-6 space-y-2 lg:col-span-9">
+      {wishBooks.map((book) => (
+        <section
+          key={book.bookId}
+          className="dark:bg-gray-100 dark:text-gray-800"
+        >
+          <div className="container max-w-6xl p-6 mx-auto space-y-6 sm:space-y-12">
+            <NavLink
+              rel="noopener noreferrer"
+              className="block max-w-sm gap-3 mx-auto sm:max-w-full group hover:no-underline focus:no-underline lg:grid lg:grid-cols-12 dark:bg-gray-50"
+            >
+              <img
+                src={book.image}
+                alt=""
+                className="object-cover h-64 w-full rounded-3xl sm:h-96 lg:col-span-3 dark:bg-gray-500"
+              />
+              <div className="p-6 space-y-2 lg:col-span-9">
                 <div className="">
-                <h2 className="card-title font-playfair text-navHeading text-2xl font-bold">
-                  {book.bookName}
-                </h2>
-                <p className="text-navHeadingEighty font-medium font-work text-base">
-                  {" "}
-                  By: {book.author}
-                </p>
-              </div>
-              <div className="flex gap-x-3 justify-start items-center">
-                Tag
-                {book.tags.map((tag, idx) => (
-                  <button key={idx} className="btn bookIdentity">
-                    {tag}
-                  </button>
-                ))}
-                <p>Year of Publishing: {book.yearOfPublishing}</p>
-              </div>
-              <div className="card-actions justify-start border-b-2">
-                <p className="text-navHeadingEighty font-medium font-work text-base">
-                  Publisher: {book.publisher}
-                </p>
-                <p className="text-navHeadingEighty font-medium font-work text-base">
-                  Page: {book.totalPages}
-                </p>
-              </div>
-              <div className="flex gap-x-3 justify-start items-center">
-                <button className="btn catagoryBtn rounded-full">Catagory: {book.category}</button>
-                <button className="btn ratingBtn rounded-full">Rating: {book.rating}</button>
-               <Link to={`/bookDetails/${book.bookId}`} >
-                <button className="btn signInBtn rounded-full">View Details</button>
-               </Link>
-              </div>
+                  <h2 className="card-title font-playfair text-navHeading text-2xl font-bold">
+                    {book.bookName}
+                  </h2>
+                  <p className="text-navHeadingEighty font-medium font-work text-base">
+                    {" "}
+                    By: {book.author}
+                  </p>
                 </div>
-              </NavLink>
-              
-              
-            </div>
-          </section>
-                
-        ))}
+                <div className="flex gap-x-3 justify-start items-center">
+                  Tag
+                  {book.tags.map((tag, idx) => (
+                    <button key={idx} className="btn bookIdentity">
+                      {tag}
+                    </button>
+                  ))}
+                  <p>Year of Publishing: {book.yearOfPublishing}</p>
+                </div>
+                <div className="card-actions justify-start border-b-2">
+                  <p className="text-navHeadingEighty font-medium font-work text-base">
+                    Publisher: {book.publisher}
+                  </p>
+                  <p className="text-navHeadingEighty font-medium font-work text-base">
+                    Page: {book.totalPages}
+                  </p>
+                </div>
+                <div className="flex gap-x-3 justify-start items-center">
+                  <button className="btn catagoryBtn rounded-full">
+                    Catagory: {book.category}
+                  </button>
+                  <button className="btn ratingBtn rounded-full">
+                    Rating: {book.rating}
+                  </button>
+                  <Link to={`/bookDetails/${book.bookId}`}>
+                    <button className="btn signInBtn rounded-full">
+                      View Details
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </NavLink>
+          </div>
+        </section>
+      ))}
     </div>
   );
 };
